60ee84b93ce8235a980d34041d4a228b8a650492
1228-Aparna <85675282+1228-Aparna@users.noreply.github.com>
2021-06-11 22:30:47 +0530

    Update Single inheritance.java

--- JP LAB/Inheritance/Single inheritance.java
+++ JP LAB/Inheritance/Single inheritance.java
@@ -1,1 +1,23 @@
 
+//default constructor using inheritance
+class Base
+{
+Base()
+{
+System.out.println("Base class constructor");
+}
+}
+class Child extends Base
+{
+Child()
+{
+System.out.println("child class constructor");
+}
+}
+class Main
+{
+public static void main(String[] args)
+{
+Child obj=new Child();
+}
+}
