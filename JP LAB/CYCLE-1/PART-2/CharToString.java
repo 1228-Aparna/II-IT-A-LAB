fb2fbd89e0a598e094d727f7d4a1ebc274b16638
1228-Aparna <85675282+1228-Aparna@users.noreply.github.com>
2021-07-05 22:48:17 +0530

    part-2 updated

---
+++ JP LAB/CYCLE-1/PART-2/CharToString.java
@@ -0,0 +1,9 @@
+class CharToString
+{
+   public static void main(String args[])
+   {
+      char ch = 'j';
+      String str = Character.toString(ch);
+      System.out.println("String is: "+str);
+   }
+}
